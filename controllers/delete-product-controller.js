d0077c50b56c40526df45fe193b76f48a1c49eba
LUUNGUYENQUOCBAO <90364030+LUUNGUYENQUOCBAO@users.noreply.github.com>
2023-03-24 21:12:17 +0700

    Initial commit

---
+++ controllers/delete-product-controller.js
@@ -0,0 +1,13 @@
+
+var session = require('./session')
+
+exports.get = function (request, response, webconfig, model) {
+    if (!session.logged(request)){
+        response.redirect(webconfig.root)
+        return
+    }
+
+    model.deleteProduct(request.query.id, function (err) {
+        response.redirect(webconfig.root + '#products')
+    })
+}
